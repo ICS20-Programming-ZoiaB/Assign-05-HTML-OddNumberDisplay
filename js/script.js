2efcc9cd90b15b45288ab6839c2a9c1d88105b86
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-05-15 21:08:43 +0000

    Added code

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// Copyright (c) 2023 ZoiaB All rights reserved
+//
+// Created by: ZoiaB
+// Created on: May 2023
+// This file contains the JS functions for index.html
+
+"use strict"
+
+function numbersSent() {
+  // initializing the numbers and message variable to an empty string
+  let numbers = "";
+  let message = "";
+
+  // getting the user values for the minimum and maximum numbers
+  let minNumber = parseInt(document.getElementById("min-number").value);
+  let maxNumber = parseInt(document.getElementById("max-number").value);
+
+  // initializing the counter variable to the minNumber
+  let counter = minNumber;
+
+  // if statement checks if minNumber > maxNumber (there is an error)
+  if (minNumber > maxNumber) {
+    message = "Ensure that the minimum is less than or equal to the maximum.";
+  }
+
+  // if statement checks if either number is negative
+  else if (minNumber < 0 || maxNumber < 0) {
+    message = "Enter positive integers for both the minimum and maximum.";
+  }
+
+  else if ((isNaN(minNumber) || (isNaN(maxNumber)))) {
+    message = "Please enter valid numbers.";
+  }
+
+  // else, complete while loop to create list of odd numbers between min and max
+  else {
+    // while loop that specifies the range
+    while (counter >= minNumber && counter <= maxNumber) {
+      // if statement checks for odd numbers, provided that the condition in the while statement is true.
+      if (counter % 2 == 1) {
+        numbers += counter + "<br>";
+      }
+      counter++;
+    }
+    message = "Here is a list of all the odd numbers between the minimum and maximum that you entered:<br><br>" + numbers;
+  }
+
+  // displaying the results (the range of odd numbers) back to the user
+  document.getElementById("results").innerHTML = message;
+}

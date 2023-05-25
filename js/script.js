27c6eac46935517698d68653b5516d8ba5c2abe6
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-05-25 21:23:02 +0000

    Added reverse option

--- js/script.js
+++ js/script.js
@@ -10,15 +10,12 @@ function numbersSent() {
   // initializing the numbers and message variable to an empty string
   let numbers = "";
   let message = "";
-  let sum = 0;
 
   // getting the user values for the minimum and maximum numbers
   let minNumber = parseInt(document.getElementById("min-number").value);
   let maxNumber = parseInt(document.getElementById("max-number").value);
 
-  // initializing the counter variable to the minNumber
-  let counter = minNumber;
-
+  // error message if user enters invalid numbers
   if ((isNaN(minNumber) || (isNaN(maxNumber)))) {
     message = "Please enter valid numbers.";
   }
@@ -28,19 +25,34 @@ function numbersSent() {
     message = "Ensure that the starting number is less than or equal to the stopping number.";
   }
 
+  //else, complete do while loop to create a reversed list of odd numbers between min and max
+  // Else, complete the while loop to create list of the even numbers
+  else {
+    const REVERSED = document.getElementById("reversed");
+    if (REVERSED.checked) {
+      let counter = maxNumber;
+      do {
+         // if statement checks for odd numbers, provided that the condition in the while statement is true. It uses an operator that determines if there is a remainder when the number is divided by 2 (method taken from https://www.tutorialspoint.com/How-to-determine-if-a-number-is-odd-or-even-in-JavaScript)
+        if (counter % 2 !== 0) {
+          numbers += counter + "<br>";
+        }
+        counter = counter - 1;
+        //while loop specifies range
+      } while (counter >= minNumber);
+      message = "Here are all the odd numbers between your selected numbers:<br><br>" + numbers;
+    }
+      
   // else, complete while loop to create list of odd numbers between min and max
   else {
-    // while loop that specifies the range
-    while (counter <= maxNumber) {
-      // if statement checks for odd numbers, provided that the condition in the while statement is true. It uses an operator that determines if there is a remainder when the number is divided by 2 (method taken from https://www.tutorialspoint.com/How-to-determine-if-a-number-is-odd-or-even-in-JavaScript)
-      if (counter % 2 == 1) {
-        numbers += counter + "<br>";
-        sum += counter;
+      let counter = minNumber;
+      while (counter <= maxNumber) {
+        if (counter % 2 !== 0) {
+          numbers += counter + "<br>";
+        }
+        counter++;
       }
-      counter++;
+      message = "Here are all the odd numbers between your selected numbers:<br><br>" + numbers;
     }
-    message = "Here are all the odd numbers between your selected numbers:<br><br>" + numbers;
-    message += "<br>Sum of all odd numbers: " + sum;
   }
 
   // displaying the results (the range of odd numbers) back to the user
